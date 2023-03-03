7bb7273daf3b25fd47272678758995682a9c3b5e
sdahal1 <raabdahl@gmail.com>
2023-03-03 06:16:46 -0800

    w1d5 study guide is up

---
+++ 5.3-higher-order-functions/5.3-study-guide.js
@@ -0,0 +1,42 @@
+/*
+1. 
+
+
+Given an array of Food items, print each item using forEach
+["Calamari", "Sweet Potato Fries", "Green Smoothie", "Fried Chicken", "Cookies", "Broccoli"]
+
+
+
+2. use foreach() to loop through an array of objects and show their details- first just the name, then a whole description
+[
+  {
+    name: "Drake",
+    fireLevel: "10",
+    moneyEarnedFromRapping: 10000000
+  },
+  {
+    name: "Lupe Fiasco",
+    fireLevel: "10",
+    moneyEarnedFromRapping: 5000000
+  },
+  {
+    name: "Rob D",
+    fireLevel: "7",
+    moneyEarnedFromRapping: -50
+  }
+];
+
+
+3. use forEach to add up the fireLevels of the artists above and give back the totalfire and averageFire
+
+4. use forEach to build a statement from given data above "My top rappers list are: Drake, Lupe Fiasco, Rob D, and they are all fuego.
+
+5. use forEach to log the moneyEarnedFromRapping for each artist above and format using the dollar sign to show amount in dollars
+
+*/
+
+
+
+
+
+
