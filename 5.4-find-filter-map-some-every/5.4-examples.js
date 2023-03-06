7926b6c91b1ad731613da0ead7cd82a838bc35d7
sdahal1 <raabdahl@gmail.com>
2023-03-06 08:23:03 -0800

    w2d1am

--- 5.4-find-filter-map-some-every/5.4-examples.js
+++ 5.4-find-filter-map-some-every/5.4-examples.js
@@ -27,6 +27,117 @@ function findParkByName(parks=[], name){
 const findParkByNameShortWay = (parks=[], name) => parks.find((element, idx)=> element.name === name)
 
 
-console.log(findParkByName(parks, "Gateway To Hawaii")) //undefined
-console.log(findParkByName(parks, "Indiana Dunes")) //  { name: "Indiana Dunes", rating: 4.1 }
+// console.log(findParkByName(parks, "Gateway To Hawaii")) //undefined
+// console.log(findParkByName(parks, "Indiana Dunes")) //  { name: "Indiana Dunes", rating: 4.1 }
 
+
+
+/* 
+.filter() -> loop through the array to find the all the elements that matches a condition and will return to you an array of these elements
+  - accepts a callback function just like .forEach and .find() does
+  - the callback function accepts parameters for element and index, just like .forEach and .find() does
+  - the callback function has to return a condition. the filter() method will stop after going through a full array
+  - .filter() will return the ARRAY that contains elements where the condition returns true on.......OR if no elements gets a truthy condition, it will return an empty array
+*/
+
+function getHighRatedParks(parks=[],rating){
+  const result =  parks.filter((element, index)=>{
+    return element.rating > rating
+  })
+
+  return result;
+}
+
+
+/* 
+[
+  { name: "Grand Canyon", rating: 5 },
+  { name: "Gateway Arch", rating: 4.5 },
+]
+
+*/
+// console.log(getHighRatedParks(parks, 4.3))
+// console.log(getHighRatedParks(parks, 5)) //[]
+
+
+/* 
+.map() -> loop through the array to transform each element to something else without modifying the original dataset
+  - accepts a callback function just like .forEach and .find() does
+  - the callback function accepts parameters for element and index, just like .forEach and .find() does
+  - the callback function can return anything that we want each element from the original array to "map" or "transform" into
+  - .map() will return the ARRAY that contains transformed elements (of whatever is returned from the callback)
+*/
+
+function getParkNames(parks = []){
+  const result =  parks.map((element, index)=>{
+
+    //whatever we return, is what the current element will be mapped to
+    return element.name
+  })
+  return result;
+}
+
+// console.log(getParkNames(parks))
+
+function getParkSummary(parks = [], rating){
+  //if the park rating is higher than given rating "ParkNameHere is great choice for you!" or if not, it will say "ParkNameHere has a low rating for you. Don't go"
+
+  const result =  parks.map((element, index)=>{
+
+    //whatever we return, is what the current element will be mapped to
+    if(element.rating > rating){
+      return `${element.name} is a great choice for you!`
+    }else{
+      return `${element.name} has a low rating for you. Don't go.`
+    }
+  })
+  return result;
+}
+
+// console.log(getParkSummary(parks, 4.3))
+
+
+
+/* 
+.some() -> loops through the array to check if "at least one" or "some" elements match the condition in the .some()'s callback function
+  - accepts a callback function just like .forEach and .find() does
+  - the callback function accepts parameters for element and index, just like .forEach and .find() does
+  - the callback function needs to return a CONDITION.
+  - .some() will return TRUE  if any of the elements match that condition in the callbackfunction  or FALSE if NONE of the elements match the condition in the cb
+*/
+
+
+function isAnyParkRatedHigherThanGivenRating(parks=[], rating){
+  const result = parks.some((element, index)=>{
+
+    //return a condition
+    return element.rating > rating
+  })
+  return result;
+}
+
+// console.log(isAnyParkRatedHigherThanGivenRating(parks, 3.8)) //true
+// console.log(isAnyParkRatedHigherThanGivenRating(parks, 5.8)) //false
+
+
+/* 
+.every() -> loops through the array to check if "every" element matches the condition in the .every()'s callback function
+  - accepts a callback function just like .forEach and .find() does
+  - the callback function accepts parameters for element and index, just like .forEach and .find() does
+  - the callback function needs to return a CONDITION.
+  - .every() will return TRUE if every one of the elements match that condition in the cb, or FALSE if even one element does not match teh condition in the cb
+*/
+
+
+function isEveryParkRatedHigherThanGivenRating(parks=[], rating){
+  const result = parks.every((element, index)=>{
+
+    //return a condition
+    return element.rating > rating
+  })
+  return result;
+}
+
+
+console.log(isEveryParkRatedHigherThanGivenRating(parks, 4.5)) //true
+// console.log(isEveryParkRatedHigherThanGivenRating(parks, 5.8)) //false
--- 5.4-find-filter-map-some-every/5.4-study-guide.js
+++ 5.4-find-filter-map-some-every/5.4-study-guide.js
@@ -52,23 +52,101 @@ const employees = [
 
 */
 
+
+const employees = [
+    {
+        name: "Lebron James",
+        salary: 110000,
+        company: {
+            name: "Google",
+            city: "Reston",
+            state: "Virginia",
+        },
+        languages: ["Javascript", "Java", "Python"],
+    },
+    {
+        name: "Scooby Doo",
+        salary: 200000,
+        company: {
+            name: "GreenChef",
+            city: "New York City",
+            state: "New York",
+        },
+        languages: ["Html", "Css"],
+    },
+    {
+        name: "Larry David",
+        salary: 80000,
+        company: {
+            name: "Comedy Central",
+            city: "Los Angeles",
+            state: "California",
+        },
+        languages: ["Javascript", "Java", "Python"],
+    },
+    {
+        name: "Beyonce",
+        salary: 90000,
+        company: {
+            name: "Google",
+            city: "Brooklyn",
+            state: "New York",
+        },
+        languages: ["Javascript", "Java", "Python"],
+    }
+];
+
 //.find() -> it returns the first item that matches a given condition, even if more than one item matches. If there is no match, find() returns undefined.
 //find an employee who is named "Larry David"
-function findEmployeeByName(employees, name){}
+function findEmployeeByName(employees=[], name){
+    const result =  employees.find((emp)=>{
 
+        //what condition do i need here?
+        return emp.name === name;
+    })
+
+    return result;
+}
+
+
+// console.log(findEmployeeByName(employees, "Larry David"))
 
 
 
 //.filter() -> This method builds a new array of only the items that match a certain condition.
 //get all the employees who are making over a given amount
-function findHighEarners(employees, amount){}
+function findHighEarners(employees, amount){
+    return employees.filter((element)=>{
+
+        //cb needs to 
+        return element.salary > amount
+    })
+}
+
+// console.log(findHighEarners(employees, 100000))
 
 
-
-
-//.map() -> this code processes each item in the array and creates a new value for each item in the original array. Each item in the original array maps to an item in the new array.
+//.map()-  this code processes each item in the array and creates a new value for each item in the original array. Each item in the original array maps to an item in the new array.
 //give back a new array containing only the company names and city for each employee in the given list
-function findCompanyNamesAndCity(employees){}
+function findCompanyNamesAndCity(employees=[]){
+    //['Google - Reston', 'GreenChef - New York City', 
+
+    return employees.map((emp)=>{
+        return `${emp.company.name} - ${emp.company.city}`
+    })
+
+    /* 
+
+    // you can also destructure in the parameter of the cb
+
+    return employees.map(({company})=>{
+        // const {company} = emp
+        return `${company.name} - ${company.city}`
+    })
+    */
+}
+
+// console.log(findCompanyNamesAndCity(employees))
 
 
 
@@ -77,16 +155,39 @@ function findCompanyNamesAndCity(employees){}
 
 
 
-//.some() -> The some() method accepts a callback function that implements a comparison that is executed for each item in the array, similar to the previous methods. If the callback function returns true for any item in the array, then the entire some() method returns true.
+
+
+//.some() - method accepts a callback function that implements a comparison that is executed for each item in the array, similar to the previous methods. If the callback function returns true for any item in the array, then the entire some() method returns true.
 //use .some() to check if any employees are from a company with the name "Comedy Central"
-function doesCompanyHaveEmployee(employees, companyName){}
+function doesCompanyHaveEmployee(employees=[], companyName){
+    const result =  employees.some((emp)=>{
+
+        //the condition we need to return
+        return emp.company.name === companyName
+    })
+
+    return result;
+}   
+
+
+// console.log(doesCompanyHaveEmployee(employees, "Comedy Central"))
 
 
 
 
-//.every() ->The every() method works by checking the condition given against every item in the array. If that condition ever fails, it will return false. Otherwise, it will return true.
+// method works by checking the condition given against every item in the array. If that condition ever fails, it will return false. Otherwise, it will return true.
 //use .every() to indicate whether every employee is making over a certain salary
-function areAllEmployeesGettingPaidGivenAmount(employees, amount){}
+function areAllEmployeesGettingPaidGivenAmount(employees=[], amount){
+    const result = employees.every((emp)=>{
+
+        return emp.salary > amount;
+    })
+
+    return result
+}
+
+console.log(areAllEmployeesGettingPaidGivenAmount(employees, 100000))
+
 
 
 
