481b8ecd814e3a953f4669b19fde6daec1f38b19
sdahal1 <raabdahl@gmail.com>
2023-04-05 18:32:27 -0700

    w5d3pm

---
+++ 20.3-selecting-elements-starter-national-parks/index.js
@@ -0,0 +1,31 @@
+
+//document.querySelector() --> to select one element;
+//document.querySelectorAll() --> to select multiple elements;
+
+
+
+
+//select the first h1 selector
+
+//select the first section
+
+//select all h1 selectors
+
+//select all sections
+
+
+//select the first section with the class of park-display
+let biscayneSection = document.querySelector(".park-display");
+
+
+//find a list of elements that are the square area in km
+
+//select all the unshaded stars
+
+//select the entire grand canyon park element
+
+//select the rating for grand canyon park
+
+
+
+
