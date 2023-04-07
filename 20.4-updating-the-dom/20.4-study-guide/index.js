2c3fe2d38261a5f56e5a33d5d33679102c140844
sdahal1 <raabdahl@gmail.com>
2023-04-07 07:55:48 -0700

    w5d5am

--- 20.4-updating-the-dom/20.4-study-guide/index.js
+++ 20.4-updating-the-dom/20.4-study-guide/index.js
@@ -1,32 +1,8 @@
-/* 
-Platform examples below
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 
 
 ASSESSMENT STUDY GUIDE BELOW
-
-
 
 
 1. addHeadingsAndImages-> make all <article>Puppy</article> look like 
@@ -57,4 +33,66 @@ let images = [
 ];
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~solutions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+function addHeadingsAndImages(){
+    //select all the article tags
+    const allArticles = document.querySelectorAll("article");
+    allArticles.forEach((articleElement,index)=>{
+        // create an h3 to insert into the article later on
+        // const h3 = `<h3>${articleElement.innerText}</h3>`;
+        const h3 = document.createElement("h3");
+        h3.innerText = articleElement.innerText;
 
+        const img = document.createElement("img");
+        img.src = images[index]
+        //insert the h3 as a child to the current articleElement in the loop
+        articleElement.innerText = "";
+        // articleElement.innerHTML += h3;
+        articleElement.appendChild(h3);
+        articleElement.appendChild(img)
+    })
+}
+
+function styleKittensandPuppies(){
+    //select all the article tags
+    const allArticles = document.querySelectorAll("article");
+    //for each article in the nodelist of allArticles, check if it has the word Puppy or Kitten in it innerText and give the appropriate class based on that
+    allArticles.forEach((articleElement)=>{
+        if(articleElement.innerText.toLowerCase().includes("puppy")){
+            articleElement.classList.add("puppy");
+        }else{
+            articleElement.classList.add("kitten");
+        }
+    })
+}
+
+
+function separateCatsFromDogs(){
+    //create the kittens section with a class of "kittens" to put kittens from the puppies section into first
+    const kittensSection = document.createElement("section")
+    kittensSection.classList.add("kittens")
+    
+    //select the element that will become the parent of the section so that we can insert the section as a child to this element
+    const rowDiv = document.querySelector(".row");
+
+    //insert the kittens section as a child to the rowDiv
+    rowDiv.appendChild(kittensSection)
+
+    //we will look at all the articles to see which article needs to be placed into the kittens section
+    const allArticles = document.querySelectorAll("article");
+
+    //select the puppies section so that we can referene the puppies section to remove kittens from puppies section
+    const puppiesSection = document.querySelector(".puppies")
+    allArticles.forEach((articleElement)=>{
+        if(articleElement.innerText.toLowerCase().includes("kitten")){
+            //remove the current articleElement if it includes the innerText of "kitten"
+            // paretnElementHere.removeChild(childElementHere)
+            puppiesSection.removeChild(articleElement)
+            kittensSection.appendChild(articleElement)
+        }
+    })
+}
+
+
+addHeadingsAndImages()
+styleKittensandPuppies()
+separateCatsFromDogs()
