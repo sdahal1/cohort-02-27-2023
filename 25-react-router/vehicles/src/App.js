4ff1e9547597e886e9fcd091a5a62b52ca7e9297
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-04-18 12:47:42 -0700

    basic vehicles example

---
+++ 25-react-router/vehicles/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+function App() {
+  const [models, setModels] = useState([])
+  const [make, setMake] = useState('honda');
+  const [input, setInput] = useState('');
+  useEffect(() => {
+    fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${make}?format=json`)
+      .then(response => response.json())
+      .then(data => setModels(data.Results))
+  }, [make])
+  return (
+    <div className="App">
+      <input type="text" onChange={(e) => setInput(e.target.value)} value={input} />
+      <button onClick={() => setMake(input)}>Search</button>
+      {models.map(model => <p key={model.Model_ID}>{model.Model_Name}</p>)}
+    </div>
+  );
+}
+
+export default App;
