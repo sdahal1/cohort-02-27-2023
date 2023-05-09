091339c806dade5e3ee864609d113b15270c61e5
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-05-09 11:31:31 -0700

    do updates, deletes, and so many selects

--- 33-sql/demo.sql
+++ 33-sql/demo.sql
@@ -25,6 +25,8 @@ VALUES
   ('Running Shoes', null),
   ('Espresso', 'nectar of the gods'),
   ('Merlot', 'for dinner');
+ 
+ 
 
 -- Read data
 -- select what from where with what extra conditions on it
@@ -35,4 +37,43 @@ SELECT item_name, created FROM items WHERE id > 4;
 SELECT * FROM items WHERE item_name = 'Running Shoes';
 
 SELECT * FROM items ORDER BY item_name LIMIT 4;
+
+-- Update data
+UPDATE items SET description = 'avocado mash' WHERE id = 13;
+
+-- Delete data
+DELETE FROM items WHERE description IS NULL;
  
+
+-- lowerCamelCase, UpperCamelCase
+-- snake_case  <-- SQL <3s snake_case
+-- caterpillar-case, kebab-case
+
+
+-- select adventures products
+SELECT * FROM products;
+SELECT COUNT(*) FROM products;
+
+SELECT * FROM products WHERE aisle = 'coffee';
+
+SELECT DISTINCT aisle FROM products;
+
+SELECT COUNT(DISTINCT aisle) FROM products;
+
+SELECT * FROM products WHERE aisle = 'tea' AND price > 30 AND price < 40;
+
+SELECT * FROM products WHERE aisle = 'tea' AND price > 30 AND price < 40 AND product_name ILIKE '%herbal%';
+
+SELECT * FROM products WHERE aisle = 'tea' AND price > 30 AND price < 40 AND product_name ILIKE '%k-cup%';
+
+SELECT * FROM products WHERE department = 'pets' ORDER BY price DESC;
+
+SELECT department, COUNT(*) FROM products GROUP BY department ORDER BY COUNT(*) DESC;
+
+SELECT COUNT(*) FROM products
+	WHERE product_name ILIKE '%canned%'
+	AND aisle NOT ILIKE '%canned%';
+
+SELECT department, ROUND(AVG(price), 2) AS average_price FROM products
+	GROUP BY department 
+	ORDER BY average_price;
