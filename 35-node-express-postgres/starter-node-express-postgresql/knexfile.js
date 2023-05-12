27755bad425653e18da3e0f7d0a61ceed8936c76
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-05-12 07:59:05 -0700

    in class demo of knex

---
+++ 35-node-express-postgres/starter-node-express-postgresql/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: "./src/db/migrations"
+    },
+    seeds: {
+      directory: "./src/db/seeds"
+    }
+  }
+}
