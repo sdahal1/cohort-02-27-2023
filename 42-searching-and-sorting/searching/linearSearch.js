854276bdf1ffb620863710e8fb3b385f1d48e1b2
kelvin711 <arauzkelvin99@gmail.com>
2023-06-06 10:12:43 -0500

    searching modules complete

---
+++ 42-searching-and-sorting/searching/linearSearch.js
@@ -0,0 +1,21 @@
+function indexOf(array, isMatch) {
+    for (let i = 0; i < array.length; i++) {
+        if (isMatch(array[i], i, array)) {
+            return i;
+        }   
+    }
+    return -1;
+}
+
+let x = [
+    {name: "Jessica", hobby: "writing"},
+    {name: "Cannon", hobby: "reading"},
+    {name: "Chad", hobby: "not sleeping and thinking about 12 finger universe"},
+]
+
+function findHobby(element, index, arr) {
+    return element.hobby === "writing";
+}
+
+console.log(indexOf(x, (element, index, arr) => element.name === "Chad" ));
+console.log(indexOf(x, findHobby ));
