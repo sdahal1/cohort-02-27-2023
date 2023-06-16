ee2bfff73bce393f82095f1372fc5c836fea7b03
kelvin711 <arauzkelvin99@gmail.com>
2023-06-16 13:16:59 -0500

    parking problem

---
+++ 43-data-structures/extra/parking.js
@@ -0,0 +1,83 @@
+const Queue = require("../queue/queue");
+
+/**
+ * Implement a Parking Lot.
+ *
+ */
+class ParkingLot {
+  constructor(capacity, rate) {
+    this.spaces = new Array(capacity).fill("vacant");
+    this.rate = rate;
+    this.revenue = 0;
+    this.queue = new Queue();
+  }
+
+  /**
+   * Returns the number of vacant parking spaces
+   * @returns {Number}
+   *  the total number of spaces where the value is "vacant".
+   */
+
+  get vacantSpaces() {
+    return this.spaces.reduce(
+      (sum, space, index) => sum + (space === "vacant" ? 1 : 0),
+      0
+    );
+  }
+
+  /**
+   * As cars enter the parking lot, the license plate number is entered and the car is parked in the first vacant space.
+   * If the lot is full, the car is added to the queue to be parked when a spot is available.
+   *
+   * @param licensePlateNumber
+   *  the license plate number of the car entering
+   */
+  enter(licensePlateNumber) {
+    // find the INDEX OF the first vacant space
+    // if there is a vacant space
+        // park the car
+    // the lot is full
+        // enqueue to our queue the car
+  }
+
+  /**
+   * As a car leaves the parking lot, or the queue, the leave method is called with the license plate number of the car leaving.
+   * @param licensePlateNumber
+   *    *  the license plate number of the car leaving.
+   */
+  leave(licensePlateNumber) {
+    // check if the car we are removing is in the lot
+    // if the car is in space
+        // remove it from the space
+        // calculate and update the revenue
+        // check if a car is in Q to take the new empty space
+        // if there is a car in Q park it
+    // if the car we want to remove is not in the lot
+        // queue empty?
+        // peek to see if its just that first one
+        // could be in the middle or end so traverse (update pointers)
+  }
+
+  /**
+   * Lists each space in the parking lot along with the license plate number of the car parked there, or
+   * "vacant" as the license plate if the spot is vacant.
+   * @returns {{licensePlateNumber: string, space: Number}[]}
+   */
+  get occupants() {
+    return this.spaces.map((licensePlateNumber, index) => ({
+      space: index + 1,
+      licensePlateNumber,
+    }));
+  }
+
+  /**
+   * The total cumulative revenue for the parking lot. The parking rate is paid when the car leaves, it does not matter how long the car stays in the spot.
+   * @returns {number}
+   *  the total revenue for the parking lot.
+   */
+  get totalRevenue() {
+    return this.revenue;
+  }
+}
+
+module.exports = ParkingLot;
